a87cd9901dac6628687beeef01ca00e677a69e8b
Edi Begovic <edibegovic@gmail.com>
2020-07-10 23:16:31 +0200

    Hammerspoon: hyper-key now toggles apps

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+local application = require 'hs.application'
+local window = require 'hs.window'
+
 local hyper = { "cmd", "alt", "ctrl", "shift" }
 
 hs.hotkey.bind(hyper, "0", function()
@@ -14,18 +17,34 @@ local applicationHotkeys = {
   e = 'Evernote',
   g = 'GitKraken',
   i = 'iTerm2',
+  t = 'iTerm2',
   w = 'Wire',
   a = 'Finder',
+  v = 'Code',
+  p = 'Preview',
+  q = 'NaN'
 }
 
-for key, app in pairs(applicationHotkeys) do
+function bindAAppHotkey(key, appName)
   hs.hotkey.bind(hyper, key, function()
-    hs.application.launchOrFocus(app)
+    local currentApp = window.frontmostWindow():application()
+    if currentApp:name() == appName then
+      currentApp:hide()
+    else
+      hs.application.launchOrFocus(appName)
+    end
   end)
 end
+
+for key, appName in pairs(applicationHotkeys) do
+  bindAAppHotkey(key, appName)
+end
+
+hs.hotkey.bind(hyper, "+", function()
+  bindAAppHotkey('q', window.frontmostWindow():application():name())
+end)
 
 hs.hotkey.bind(hyper, "return", function()
   hs.application.launchOrFocus('Evernote')
   hs.eventtap.keyStroke("cmd", "j")
 end)
-
