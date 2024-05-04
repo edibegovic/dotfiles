34044d13e7789610a291e8f8ec46d51e08af4ab7
Edi <edibegovic@gmail.com>
2024-05-04 22:29:46 +0200

    More plugings

--- .config/nvim/lua/edi/plugins/plugins.lua
+++ .config/nvim/lua/edi/plugins/plugins.lua
@@ -1,18 +1,17 @@
 return { 
-  { 'mg979/vim-visual-multi' },
+  --{ 'mg979/vim-visual-multi' },
   { 'wellle/targets.vim' },
-  -- commentry and map s (in normal mode) to gcc and in visual mode to gc
   { 'tpope/vim-commentary',
-    config = function()
-      vim.api.nvim_set_keymap('n', 's', 'gcc', { noremap = true })
-      vim.api.nvim_set_keymap('v', 's', 'gc', { noremap = true })
-    end
-  },
-  { 'tpope/vim-surround' },
-  { 'wellle/targets.vim' },
-  { 'machakann/vim-highlightedyank',
-    config = function()
-      vim.g.highlightedyank_highlight_duration = 100
-    end
+  config = function()
+    vim.keymap.set('n', 's', 'gcc', { noremap = true, remap = true })
+    vim.keymap.set('v', 's', 'gcgv', { noremap = true, remap = true })
+  end
+},
+{ 'tpope/vim-surround' },
+{ 'wellle/targets.vim' },
+{ 'machakann/vim-highlightedyank',
+config = function()
+  vim.g.highlightedyank_highlight_duration = 100
+end
   },
 }
